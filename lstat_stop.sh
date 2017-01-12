22c8798c4166dbfd3644a310eeae168f97eb8e7f
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2017-01-12 19:21:53 +0900

    Fix process name to kill

--- lstat_stop.sh
+++ lstat_stop.sh
@@ -6,7 +6,7 @@ if [ -f "$PIDFILE" ]; then
     while read PID; do
         COMMAND=$(ps -p $PID -o comm=)
         
-        if [ "$COMMAND" = 'python' -o "$COMMAND" = 'perl' ]; then
+        if [ "$COMMAND" = 'python' -o "$COMMAND" = 'dstat' -o "$COMMAND" = 'perl' ]; then
             kill $PID
         fi
     done <$PIDFILE
